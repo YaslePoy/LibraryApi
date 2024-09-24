42feae0a3a339824659f458861aa7676e1e081d0
Mikhail Mitrofanov <mmm606@inbox.ru>
2024-09-24 10:27:34 +0300

    очищен юзер контроллер

--- LibApi/Controllers/UsersController.cs
+++ LibApi/Controllers/UsersController.cs
@@ -17,44 +17,7 @@ namespace LibApi.Controllers
         {
             _context = context;
         }
-
-        // [HttpGet]
-        // [Route("getAllUsers")]
-        // public async Task<IActionResult> GetAllUsers()
-        // {
-        //     var users = await _context.Logins.Where(a => a.id_Login == 1).Include(a => a.Users).ToListAsync();
-        //      
-        //     return new OkObjectResult(new
-        //     {
-        //         users = users,
-        //         status = true
-        //     });
-        // }
-        //
-        // [HttpPost]
-        // [Route("createNewUserAndLogin")]
-        // public async Task<IActionResult> CreateNewUserAndLogin(CreateNewUserAndLogin newUser)
-        // {
-        //     var user = new User()
-        //     {
-        //         Name = newUser.Name,
-        //         Description = newUser.Description,
-        //     };
-        //
-        //     await _context.Users.AddAsync(user);
-        //     await _context.SaveChangesAsync();
-        //
-        //     var login = new Logins()
-        //     {
-        //         User_id = user.id_User,
-        //         Login = newUser.Login,
-        //         Password = newUser.Password,
-        //     };
-        //
-        //     await _context.Logins.AddAsync(login);
-        //     await _context.SaveChangesAsync();
-        //
-        //     return Ok();
-        // }
+        
+        
     }
 }
--- LibApi/DataBaseContext/LibApiContext.cs
+++ LibApi/DataBaseContext/LibApiContext.cs
@@ -11,5 +11,10 @@ namespace LibApi.DataBaseContext
         }
 
         public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<BookCopy> BookCopies { get; set; }
+        public DbSet<BookRental> BookRentals { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<BooksGenre> BooksGenres { get; set; }
     }
 }
