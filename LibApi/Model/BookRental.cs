3c75fe3b93537ea62787d364012e59266e84d57f
Mikhail Mitrofanov <mmm606@inbox.ru>
2024-09-24 10:20:43 +0300

    вроде последняя моделька

---
+++ LibApi/Model/BookRental.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibApi.Model;
+
+public class BookRental : DbEntity
+{
+    [ForeignKey("User")]
+    public int? UserId { get; set; }
+    public User? User { get; set; }
+    
+    [ForeignKey("BookCopy")]
+    public int BookCopyId { get; set; }
+    public BookCopy BookCopy { get; set; }
+    public DateTime RentStart { get; set; }
+    public DateTime RentEnd { get; set; }
+}
